b46d06f828f3264d97353e435f0b7f70c65f892b
kodebae <karmenuopx@outlook.com>
2020-08-10 17:08:21 -0400

    set up complete

---
+++ src/main/java/javaorders/kodebae/KodebaeApplication.java
@@ -0,0 +1,13 @@
+package javaorders.kodebae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodebaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodebaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javaorders/kodebae/KodebaeApplicationTests.java
@@ -0,0 +1,13 @@
+package javaorders.kodebae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodebaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

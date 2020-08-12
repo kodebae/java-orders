a512e44fb35aad613064b045c04a19f6a4f85a89
kodebae <karmenuopx@outlook.com>
2020-08-11 21:06:11 -0400

    updated order model

---
+++ src/main/java/javaorders/day1/models/Customers.java
@@ -0,0 +1,9 @@
+package javaorders.day1.models;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Customers {
+}
